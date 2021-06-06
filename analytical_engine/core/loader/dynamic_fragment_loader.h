24b85f0dc0a9ea0fb50fb3469d4be0c0e5627b0f
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2021-06-06 17:34:07 +0800

    format

--- analytical_engine/core/loader/dynamic_fragment_loader.h
+++ analytical_engine/core/loader/dynamic_fragment_loader.h
@@ -1,10 +1,29 @@
 
+/** Copyright 2020 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef ANALYTICAL_ENGINE_CORE_LOADER_DYNAMIC_FRAGMENT_LOADER_H_
+#define ANALYTICAL_ENGINE_CORE_LOADER_DYNAMIC_FRAGMENT_LOADER_H_
+
 #include <memory>
 #include <string>
+#include <vector>
 
-#include <grape/io/local_io_adaptor.h>
-#include <grape/fragment/basic_fragment_loader.h>
-#include <grape/grape.h>
+#include "grape/fragment/basic_fragment_loader.h"
+#include "grape/grape.h"
+#include "grape/io/local_io_adaptor.h"
 
 #include "core/fragment/dynamic_fragment.h"
 
@@ -79,7 +98,7 @@ class DynamicFragmentLoader {
       }
     }
     fragment->Init(comm_spec_.worker_id(), vertices, edges, spec.directed,
-                  true);
+                   true);
     return fragment;
   }
 
@@ -88,3 +107,4 @@ class DynamicFragmentLoader {
   line_parser_t line_parser_;
 };
 }  // namespace gs
+#endif  // ANALYTICAL_ENGINE_CORE_LOADER_DYNAMIC_FRAGMENT_LOADER_H_
