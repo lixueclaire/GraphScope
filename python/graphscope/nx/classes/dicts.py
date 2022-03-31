829da1470f9a8e773af7fd9b22518c8e6aaa1bed
acezen <weibin.zen@gmail.com>
2022-03-31 11:22:06 +0800

    Update

--- python/graphscope/nx/classes/dicts.py
+++ python/graphscope/nx/classes/dicts.py
@@ -35,7 +35,6 @@ __all__ = ["NodeDict", "AdjDict", "NodeCache", "AdjListCache"]
 class Cache:
     def __init__(self, graph):
         self.parser = simdjson.Parser()
-        self.nbr_parser = simdjson.Parser()
         self._graph = graph
         self._node_id_cache = None
         self._node_attr_cache = None
@@ -109,24 +108,25 @@ class Cache:
                 break
             t = timer()
             f = self.future.pop()
+            # archive = f.result()
+            # self.gid = archive.get_uint64()
+            # node_size = archive.get_uint32()
+            self.gid, node_size, self._node_id_cache = f.result()
             print("pop result", timer() - t)
-            archive = f.result()
-            self.gid = archive.get_uint64()
             print("self.gid", self.gid)
-            node_size = archive.get_uint32()
             self.n += node_size
             print("self.n", self.n)
             if self.n < self._len:
                 self._fetch_node_id_cache(self.gid)
             self.node_attr_align = self.neighbor_align = self.neighbor_attr_align = False
             self.pre_gid = self.gid
-            t = timer()
-            array = self.parser.parse(archive.get_bytes())
-            print("parse node cache", timer() - t)
-            t = timer()
-            self._node_id_cache = {k: v for v, k in enumerate(array)}
-            print("to hash map", timer() - t)
-            del array
+            # t = timer()
+            # self._node_id_cache = self.parser.parse(archive.get_bytes())
+            # print("parse node cache", timer() - t)
+            # t = timer()
+            # self._node_id_cache = {k: v for v, k in enumerate(array)}
+            # print("to hash map", timer() - t)
+            # del array
             for n in self._node_id_cache:
                 yield n
 
@@ -153,7 +153,13 @@ class Cache:
         op = dag_utils.report_graph(self._graph, types_pb2.NODE_ID_CACHE_BY_GID, gid=gid)
         archive = op.eval()
         print("fetch cache", timer() - t)
-        return archive
+        gid = archive.get_uint64()
+        node_size = archive.get_uint32()
+        t = timer()
+        f = io.BytesIO(archive.get_bytes())
+        node_id_cache = simdjson.loads(f)
+        print("Loads node cache", timer() - t)
+        return gid, node_size, node_id_cache
 
     def _get_node_attr_cache(self, gid):
         print("call _get_data_cache", gid)
